b0d17fcf40801593fa80de355e1103e076b906a6
Arseny Grunin <grunin200412@gmail.com>
2021-07-27 16:43:30 +0300

    Toast

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -62,8 +62,28 @@ function checkInLike(goodID){
 }
 
 
+/* support func */
+function cartAnimation() {
+	document.querySelector(".icon-cart svg").classList.add('add-to-cart-animation');	
+	setTimeout(() => {
+		document.querySelector(".icon-cart svg").classList.remove('add-to-cart-animation');
+	}, 2100);
+}
+
+function showToast() {
+	let a = new bootstrap.Toast(document.getElementById('Toast'));
+    a.show();
+}
+
+function addToCartSelect() {
+
+}
+
+
+
 /* init */
 function itemPageInit(){
+
 	/*Size select*/
 	for (var i = 0; i < document.getElementById("sizeList").children.length; i++) {
 		let sizeList = document.getElementById("sizeList");
@@ -110,11 +130,7 @@ function itemPageInit(){
 
 	    		//addToCartSelect();
 
-	    		document.querySelector(".icon-cart svg").classList.add('add-to-cart-animation');
-	    		
-	    		setTimeout(() => {
-	    			document.querySelector(".icon-cart svg").classList.remove('add-to-cart-animation');
-	    		}, 2100);
+	    		cartAnimation()
 	    	};
 	    }
     }
@@ -140,10 +156,6 @@ function itemPageInit(){
 	    	likeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/><path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/></svg>';
 	    }
     }
-
-}
-
-function addToCartSelect() {
 
 }
 
