beba895e08b15b30f8bf077609a1635ced20fb21
Arseny Grunin <grunin200412@gmail.com>
2021-07-31 22:52:49 +0300

    Search order

--- application/views/admin/orders.php
+++ application/views/admin/orders.php
@@ -1,8 +1,21 @@
 <?php if ($orders): ?>
+<div class="row mb-3">
+  <div class="col-10">
+    <div class="form-floating">
+        <input type="text" name="number" class="form-control" id="numberSearch" placeholder="Номер заказа">
+        <label for="numberSearch">Номер заказа</label>
+    </div>
+  </div>
+  <div class="col-2">
+    <button class="btn btn-outline-dark w-100 h-100" type="button" onclick="document.location = '/admin/orders/'+document.getElementById('numberSearch').value;">Найти</button>
+  </div>
+</div>
+
 <table class="table table-hover">
   <thead>
     <tr>
       <th scope="col">#</th>
+      <th scope="col">Номер заказа</th>
       <th scope="col">Дата заказа</th>
       <th scope="col">Тип заказа</th>
     </tr>
@@ -10,9 +23,10 @@
   <tbody>
     <?php foreach ($orders as $key => $value): ?>
       <tr onclick="document.location = '/admin/orders/<?php echo $value['id']; ?>';">
-        <th scope="row"><?php echo $value['id']; ?></th>
+        <th scope="row"><?php echo $key+1; ?></th>
+        <th scope="row" ><?php echo $value['id']; ?></th>
         <td><?php echo $value['ordertime']; ?></td>
-        <td><?php if ($value['deliverytype'] == 1) echo 'Доставка'; if ($value['deliverytype'] == 2) echo 'Самомвывоз'; ?></td>
+        <td><?php if ($value['deliverytype'] == 1) echo 'Доставка'; if ($value['deliverytype'] == 2) echo 'Самовывоз'; ?></td>
       </tr>
     <?php endforeach ?>
   </tbody>
