882bd91cd2b89e45053ae78f42623ece6df56f05
Arseny Grunin <grunin200412@gmail.com>
2021-08-05 16:16:26 +0300

    Comment

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -3,11 +3,11 @@
   <a href="/admin/settings" class="list-group-item list-group-item-action">Настройки</a>
   <a href="/admin/orders" class="list-group-item list-group-item-action">Заказы</a>
 
-
+  <!--
   <a href="/admin/headers" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">See headers</a>
   <a href="/admin/databasedebug" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Database debug</a>
   <a href="/admin/fillucode" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Fill ucode</a>
-  <a  class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</a>
+  <a  class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</a>-->
 </div>
 
 <?php 
