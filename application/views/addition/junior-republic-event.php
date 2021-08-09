938ac531ca440e748309a8ce18e47f036d675c37
Machigai <grunin200412@gmail.com>
2021-08-10 01:12:09 +0300

    Cart info svg

--- application/views/addition/junior-republic-event.php
+++ application/views/addition/junior-republic-event.php
@@ -1,2 +1,4 @@
 <img src="/assets/img/main/0052.webp" class="d-none d-sm-block w-100" alt="...">
 <img src="/assets/img/main/0054.jpg" class="d-block d-sm-none w-100" alt="..."> 
+<h1>Акция от Junior Republic</h1>
+      <h6>Купите комплект одежды Junior Republic и получите стильный рюкзак в подарок</h6>
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,12 +1,32 @@
 <link href="/assets/css/homepage-style.css" rel="stylesheet">
 
 <div class="col col-12 home">
-  <a href="/junior-republic-event">
-    <video loop autoplay muted style="max-width:100%; height:85vh;">
-      <source src="/assets/video/back-to-school.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-  </a>
+  <div class="mb-5" >
+      <a href="/junior-republic-event">
+        <video loop autoplay muted style="height: 85vh; max-width: 100%;" class="d-block d-md-none">
+          <source src="/assets/video/back-to-school-portrait2.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+        <video loop autoplay muted style="height: 85vh; max-width: 100%;" class="d-none d-md-block">
+          <source src="/assets/video/back-to-school-landscape.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </a>
+  </div>
+  <div class="row">
+    <div class="col col-12 col-md-8">
+      <a href="/junior-republic-event">
+        <img class="hover-scale" src="https://www.productionparadise.com/newsletters/641/photos/39525/original/achim-lippoth-ckj-kids-spread-f11-1.jpg" alt="">
+      </a>
+    </div>
+    <div class="col col-12 col-md-4">
+      <h1>Акция от Junior Republic</h1>
+      <h6>Новая акция в предверии школьного сезона, Купите комплект одежды Junior Republic и получите стильный рюкзак в подарок</h6>
+      <a href="/junior-republic-event" class="look">Смотреть</a>
+    </div>
+  </div>
+    
+  
     <!-- Carusel -->
     <!--
     <div id="homeCarousel" class="carousel carousel-dark slide mb-3" data-bs-ride="carousel" data-bs-interval="false">
