5179c5e949500579e275abd3392d0a5688bdd93d
Machigai <grunin200412@gmail.com>
2021-08-10 13:02:04 +0300

    Add price helper

---
+++ application/helpers/price_helper.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// ------------------------------------------------------------------------
+
+if ( ! function_exists('get_price'))
+{
+	function get_price($price, $sale = 0){
+        return number_format($price * (0.01 * (100 - $sale)), 0,"."," ");
+    }
+}
